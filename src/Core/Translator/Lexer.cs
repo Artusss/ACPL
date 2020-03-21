14dff72092c3816e5ef6f9049765151209ccc8fc
Artusss <volkovs.a@mail.ru>
2020-03-21 21:20:49 +0300

    mood

--- src/Core/Translator/Lexer.cs
+++ src/Core/Translator/Lexer.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace src.Core.Translator
 {
     class Lexer
     {
+        private Dictionary<string, string> Variables;
+
+        public Lexer()
+        {
+
+        }
+
+        private void setVar(List<string> curLexem_imp)
+        {
+            var varKey = curLexem_imp.First();
+            if (Variables.ContainsKey(varKey))
+            {
+
+                return;
+            }
+        }
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace src
 {
+    public enum ResWords
+    {
+        INT = 'i',
+        DOUBLE = 'd',
+        STRING = 's',
+        LIST = 'l',
+        HAVE = '=',
+        PLUS = '+',
+        MINUS = '-',
+        MULT = '*',
+        SPLIT = '/',
+        LP = '(',
+        RP = ')',
+        PRINT = ':'
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Regex regex;
+            string plus_pattern = $"";
+            Console.WriteLine(ResWords.PLUS);
+            string rawExp = "5 + 6 * ( 9 - 4 )";
+
+
             Console.WriteLine("Hello World!");
         }
+
+
     }
 }
